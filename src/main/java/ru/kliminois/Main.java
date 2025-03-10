897d8243d2842d005348d44ad24ef6df663edbb4
kliminois <klimas.vlad@yandex.ru>
2025-03-10 21:56:37 +0300

    added getMessage method

--- src/main/java/ru/kliminois/Main.java
+++ src/main/java/ru/kliminois/Main.java
@@ -2,6 +2,10 @@ package ru.kliminois;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+        System.out.printf(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello world!";
     }
 }
